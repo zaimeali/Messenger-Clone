473bf89440760b6ac45266ed49ddb6ddfc1b21ca
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-16 16:20:33 +0500

    Showing Message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput("");
+  }
+
   return (
     <div className="App">
-    
+      <input value={ input } onChange={(event) => setInput(event.target.value)} />
+      <button onClick={ sendMessage }>Send Message</button>
+      {/* <p>{ input }</p> */}
+      {/* <p>{ messages }</p> */}
+
+      { messages.map(message => (
+        <p>{ message }</p>
+      )) }
     </div>
   );
 }
