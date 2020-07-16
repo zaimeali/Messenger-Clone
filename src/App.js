0f1d1d9c2127e189d0f87e6dcac49d1b304d2d30
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-16 16:40:02 +0500

    Material Button and Input added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+
 function App() {
 
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
 
   const sendMessage = (event) => {
+    event.preventDefault();
     setMessages([...messages, input]);
     setInput("");
   }
 
   return (
     <div className="App">
-      <input value={ input } onChange={(event) => setInput(event.target.value)} />
-      <button onClick={ sendMessage }>Send Message</button>
+      <FormControl onSubmit={ sendMessage }>
+        <InputLabel>Enter a message...</InputLabel>
+        <Input 
+          value={ input } 
+          onChange={(event) => setInput(event.target.value)} 
+        />
+        <Button 
+          color="primary" 
+          variant="contained" 
+          type="submit"
+          disabled={ !input }
+        >
+          Send Message
+        </Button>
+      </FormControl>
       {/* <p>{ input }</p> */}
       {/* <p>{ messages }</p> */}
 
